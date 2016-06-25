c9ca40916de042d1806ad7a279b55b1a611f8231
Della Anjeh <dellaanjeh@Dellas-MacBook-Pro.local>
2016-06-24 20:07:55 -0500

    adds incomplete splash activity and more drawables

--- app/src/main/java/com/example/dellaanjeh/todonutapp/SplashActivity.java
+++ app/src/main/java/com/example/dellaanjeh/todonutapp/SplashActivity.java
@@ -1,19 +1,31 @@
 package com.example.dellaanjeh.todonutapp;
 
+import android.support.v7.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 3000;
+    TextView tvTitle, tvDonut;
+    ActionBar bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        //bar = getActionBar();
+//        bar.hide();
+        tvDonut = (TextView) findViewById(R.id.tvDonut);
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        Typeface sifonn = Typeface.createFromAsset(getAssets(), "fonts/SIFONN_BASIC.otf");
+        Typeface sifonnOutline = Typeface.createFromAsset(getAssets(), "fonts/SIFONN_BASIC_OUTLINE.otf");
+        tvDonut.setTypeface(sifonnOutline);
+        tvTitle.setTypeface(sifonn);
         new Handler().postDelayed(new Runnable() {
 
             /*
